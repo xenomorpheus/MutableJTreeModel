d41901b73526206a4717e4900023bf4fbf36cfbf
xenomorpheus <xenomorpheus@users.noreply.github.com>
2023-07-11 21:22:40 +0930

    Automatically remove nodes after all added.

--- src/main/java/com/example/mutablejtreemodel/Main.java
+++ src/main/java/com/example/mutablejtreemodel/Main.java
@@ -1,8 +1,8 @@
 /** This document is AS-IS. No claims are made for suitability for any purpose. */
 package com.example.mutablejtreemodel;
 
+import java.util.ArrayDeque;
 import javax.swing.SwingUtilities;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -35,21 +35,32 @@ public class Main {
 			}
 		});
 
-
 		// Keep creating child nodes within the last created child node
 		// and within the root node.
+		ArrayDeque<Node> nodes = new ArrayDeque<>();
 		for (int i = 0; i < 10; i++) {
 			// TODO fix - root.setName("Root Node Name "+i);
 			for (int j = 0; j < 2; j++) {
-				logger.info("********************************************");
+				var name = "CHILD_NODE_" + i + "_" + j;
+				logger.info("Adding node: "+name);
 				Thread.sleep(1000);
-				child = new Node("CHILD_NODE_" + i + "_" + j);
+				child = new Node(name);
 				parent.add(child);
+				nodes.addLast(child);
 			}
 			parent = child;
-			// Also add children to root. Just for fun.
-			root.add(new Node("CHILD_NODE_" + i));
 
+			// Also add children to root. Just for fun.
+			var child_of_root = new Node("ROOT_CHILD_NODE_" + i);
+			root.add(child_of_root);
+			nodes.addLast(child_of_root);
+		}
+
+		while(!nodes.isEmpty()){
+			var node = nodes.removeLast();
+			logger.info("Removing node: "+node);
+			node.destroy();
+			Thread.sleep(1000);
 		}
 	}
 }
