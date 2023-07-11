a6e9307f2efddce8db8c49aea3d045ab6cb3f667
xenomorpheus <xenomorpheus@users.noreply.github.com>
2023-07-11 20:10:26 +0930

    Incorporate NodeJTreeModelDemo

--- src/main/java/com/example/mutablejtreemodel/Main.java
+++ src/main/java/com/example/mutablejtreemodel/Main.java
@@ -3,24 +3,52 @@ package com.example.mutablejtreemodel;
 
 import javax.swing.SwingUtilities;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Main program. Create a root node and start a tree editor.
- * 
+ *
  * @author xenomorpheus
  * @version $Revision: 1.0 $
  */
 public class Main {
+	/** class logger */
+	private static final Logger logger = LogManager.getLogger(Main.class);
+
+
 	/**
 	 * Method main.
-	 * 
-	 * @param args
+	 *
+	 * @param argv
 	 *            Command line arguments. Not used.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] argv) throws InterruptedException {
+		final Node root = new Node("Root Node");
+		Node parent = root;
+		Node child = null;
+
+		// The UI is on a different thread to the one that changes
+		// the Model.
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				new NodeJTreeEditor(new Node("Root Node"));
+				new NodeJTreeEditor(root);
 			}
 		});
+
+		// Keep creating child nodes within the last created child node.
+		for (int i = 0; i < 10; i++) {
+			// TODO fix - root.setName("Root Node Name "+i);
+			for (int j = 0; j < 2; j++) {
+				logger.info("********************************************");
+				Thread.sleep(1000);
+				child = new Node("CHILD_NODE_" + i + "_" + j);
+				parent.add(child);
+			}
+			parent = child;
+			// Also add children to root. Just for fun.
+			root.add(new Node("CHILD_NODE_" + i));
+
+		}
 	}
 }
