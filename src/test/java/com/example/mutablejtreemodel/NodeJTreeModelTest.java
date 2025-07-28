389cc4c20fecf0574d196384a3f16bddc9d411ca
xenomorpheus <xenomorpheus@users.noreply.github.com>
2025-07-28 20:06:09 +0930

    Fix unused variable

--- src/test/java/com/example/mutablejtreemodel/NodeJTreeModelTest.java
+++ src/test/java/com/example/mutablejtreemodel/NodeJTreeModelTest.java
@@ -14,6 +14,7 @@ public class NodeJTreeModelTest {
   @Test
   public void testNodeJTreeModel() {
     AbstractTreeModel model = new NodeJTreeModel();
+    assertEquals("NodeJTreeModel", model.getClass().getSimpleName());
   }
 
   @Test
